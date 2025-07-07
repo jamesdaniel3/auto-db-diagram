4a2aa67b9832ce2cd58c594f409a8fea7cee1f83
James Daniel <114611694+jamesdaniel3@users.noreply.github.com>
2025-07-06 20:21:01 -0400

    add the ability for the check_tool error to pass a custom message

--- lib/tools_check.sh
+++ lib/tools_check.sh
@@ -2,6 +2,9 @@
 
 check_tool() {
     if ! command -v "$1" &>/dev/null; then
+        if [ -n "$2" ]; then
+            error "$2"
+        fi 
         error "$1 is not installed or not in PATH"
     fi
 }
